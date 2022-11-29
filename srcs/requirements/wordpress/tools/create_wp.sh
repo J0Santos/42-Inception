0b25f90838899409622ea2f7094dfd8961c3c3ff
Ze <zepedroabsantos@gmail.com>
2022-11-29 15:39:12 +0000

    wordpress finally working

--- srcs/requirements/wordpress/tools/create_wp.sh
+++ srcs/requirements/wordpress/tools/create_wp.sh
@@ -12,14 +12,16 @@ else
 	#update config file
 	rm -rf /etc/php/7.3/fpm/pool.d/www.conf
 	mv /tmp/www.conf /etc/php/7.3/fpm/pool.d/
+	rm -rf /var/www/wordpress/wp-config.php
+	mv /tmp/wp-config.php /var/www/wordpress/
 
 	#Import env vars into config file
-	cd /var/www/html/wordpress
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	mv wp-config-sample.php wp-config.php
+	#cd /var/www/html/wordpress
+	#sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+	#sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+	#sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+	#sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+	#mv wp-config-sample.php wp-config.php
 fi
 
 service php7.3-fpm start
